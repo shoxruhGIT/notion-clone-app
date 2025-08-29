0d677656c4402cdde19b4bdbf7d88a861aee7588
shoxaka <shnarzullayevich@gmail.com>
2025-08-29 20:37:30 +0500

    fx

--- app/_not-found.tsx
+++ app/_not-found.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 
-const Error = () => {
+const NotFound = () => {
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
       <Image
@@ -30,4 +30,4 @@ const Error = () => {
   );
 };
 
-export default Error;
+export default NotFound;
